6bc839a48497bf9ce1457d295c32806efd4d8ced
Cliodhna Nee <g00386395@atu.ie>
2022-11-01 23:26:09 +0000

    Added title class with testing

--- src/main/java/ie/atu/Passenger.java
+++ src/main/java/ie/atu/Passenger.java
@@ -1,4 +1,14 @@
 package ie.atu;
 
 public class Passenger {
+    String title;
+
+    public String title(String t) {
+        this.title = t;
+
+        if(t!="Mr" && t!="Mrs" && t!="Ms"){
+            throw new IllegalArgumentException("Error, Invalid Title! Options: Mr, Mrs, Ms");
+        }
+        return t;
+    }
 }
